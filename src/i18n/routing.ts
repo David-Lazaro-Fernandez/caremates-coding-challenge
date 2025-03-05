b38de67643bb4fdf6dbff0efb285a8376a2dc0b3
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2025-03-05 22:25:28 +0000

    [INIT] Project structure and base languages.

---
+++ src/i18n/routing.ts
@@ -0,0 +1,18 @@
+import {defineRouting} from 'next-intl/routing';
+
+export const routing = defineRouting({
+  locales: ['en', 'de', 'es', 'fr'],
+  defaultLocale: 'en',
+  pathnames: {
+    '/': '/',
+    '/pathnames': {
+      en: '/pathnames',
+      es: '/pathnames',
+      fr: '/pathnames',
+      de: '/pfadnamen'
+    }
+  }
+});
+
+export type Pathnames = keyof typeof routing.pathnames;
+export type Locale = (typeof routing.locales)[number];
