1eb61e443d3ecd4d0eaa865a87c0844189071f3d
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2025-03-06 07:11:04 +0000

    [HOTFIX] Redirect

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,7 @@
 import { getLocale } from 'next-intl/server';
-import {setRequestLocale} from 'next-intl/server';
 import { redirect } from 'next/navigation';
-
-
 
 export default async function IndexPage() {
   const locale = await getLocale()
-  setRequestLocale(locale);
-  redirect( `${locale}/start`)
-  return (
-    <></>
-  );
+  return redirect( `${locale}/start`)
 }
